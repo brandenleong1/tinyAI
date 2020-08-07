abca51d1ca3052b548d7131f1dbee0c254b7368a
brandenleong1 <branden.leong.1@gmail.com>
2020-08-06 21:26:30 -0700

    Version 1.2.1
    Removed fixed function feature in logic_simulation.py
    Replaced fixed function with randomized function in logic_simulation.py
    Modified artifical_intelligence.py to be more modular and accept random functions from logic_simulation.py

--- artificial_intelligence.py
+++ artificial_intelligence.py
@@ -1,5 +1,5 @@
 import numpy as np
-from logic_simulation import logicsim_inputs, logicsim_outputs
+from logic_simulation import logicsim_inputs, logicsim_outputs, logicsim_output_possibilities, init
 # from image_converter import imgconvert_inputs, imgconvert_outputs
 
 
@@ -65,15 +65,12 @@ def simulate(input):
 if __name__ == '__main__':
     np.random.seed(1)  # TODO
 
-    output_possibilities = np.array([0, 1, 10, 11, 100, 101, 110, 111, 1000, 1001, 1010, 1011, 1100, 1101, 1110, 1111, 10000, 10001, 10010, 10011, 10100, 10101, 10110, 10111, 11000, 11001, 11010, 11011, 11100, 11101, 11110, 11111,
-                                     100000, 100001, 100010, 100011, 100100, 100101, 100110, 100111, 101000, 101001, 101010, 101011, 101100, 101101, 101110, 101111, 110000, 110001, 110010, 110011, 110100, 110101, 110110, 110111, 111000, 111001, 111010, 111011, 111100, 111101, 111110, 111111,
-                                     1000000, 1000001, 1000010, 1000011, 1000100, 1000101, 1000110, 1000111, 1001000, 1001001, 1001010, 1001011, 1001100, 1001101, 1001110, 1001111, 1010000, 1010001, 1010010, 1010011, 1010100, 1010101, 1010110, 1010111, 1011000, 1011001, 1011010, 1011011, 1011100, 1011101, 1011110, 1011111,
-                                     1100000, 1100001, 1100010, 1100011, 1100100, 1100101, 1100110, 1100111, 1101000, 1101001, 1101010, 1101011, 1101100, 1101101, 1101110, 1101111, 1110000, 1110001, 1110010, 1110011, 1110100, 1110101, 1110110, 1110111, 1111000, 1111001, 1111010, 1111011, 1111100, 1111101, 1111110, 1111111])  # TODO
+    output_possibilities = np.array(logicsim_output_possibilities)  # TODO
 
-    training_set_inputs = logicsim_inputs[np.arange(len(logicsim_inputs)) < 61847529062]  # TODO
-    training_set_outputs = logicsim_outputs[np.arange(len(logicsim_outputs)) < 61847529062]  # TODO
+    training_set_inputs = logicsim_inputs[np.arange(len(logicsim_inputs)) < (len(logicsim_inputs) * 0.9)]  # TODO
+    training_set_outputs = logicsim_outputs[np.arange(len(logicsim_outputs)) < (len(logicsim_outputs) * 0.9)]  # TODO
 
-    layers = {1: [36, 16], 2: [16, 16], 3: [16, 128]}  # TODO
+    layers = {1: [init[2], 16], 2: [16, 16], 3: [16, init[3]]}  # TODO
     layer_weights = {}
     layer_outputs = {}
     layer_error = {}
